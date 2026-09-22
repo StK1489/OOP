a2323592dcaa7544d97f75eb03ba219324b4cd70
Vsevolod Kuznetsov <v.kuznetsov12@g.nsu.ru>
2026-09-22 23:59:52 +0700

    Удалил дубликаты из корня, оставил только папки задач

--- run.ps1
+++
@@ -1,4 +0,0 @@
-chcp 65001 | Out-Null
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-javac -encoding UTF-8 -d out src/main/java/ru/nsu/vkuznetsov/task_1_1_2/domain/*.java src/main/java/ru/nsu/vkuznetsov/task_1_1_2/game/*.java src/main/java/ru/nsu/vkuznetsov/task_1_1_2/ui/*.java src/main/java/ru/nsu/vkuznetsov/task_1_1_2/Main.java
-java -cp out ru.nsu.vkuznetsov.task_1_1_2.Main
--- src/main/java/ru/nsu/vkuznetsov/task_1_1_2/domain/Gender.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.vkuznetsov.task_1_1_2.domain;
-
-/**
- * Род существительного, используемый для склонения названий карт.
- */
-public enum Gender {
-    MASCULINE,
-    FEMININE
-}

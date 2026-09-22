a7898d9776de2e58f6eb6b7012c38e6164505acf
StK1489 <StK1489@users.noreply.github.com>
2026-09-22 17:05:14 +0000

    Deploying to gh-pages from @ StK1489/OOP@bbfa47f94a7ff3960a60907e1bce4c0bbe3e5b8e 🚀

---
+++ Task_1_1_2/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.vkuznetsov.task_1_1_2"},{"l":"ru.nsu.vkuznetsov.task_1_1_2.domain"},{"l":"ru.nsu.vkuznetsov.task_1_1_2.game"},{"l":"ru.nsu.vkuznetsov.task_1_1_2.ui"}];updateSearchResults();
